646c8ffe9b9a38725638f8c4889651958c78ae7e
Anton Berezhnyi <aberezhniy@clips4sale.com>
2022-12-20 23:30:48 +0200

    Add signals handling

--- mainloop.go
+++ mainloop.go
@@ -4,6 +4,9 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -12,16 +15,22 @@ var TooManyError = errors.New("too many error")
 
 func runMainLoop(config Config, out io.Writer, iterationExecutor func() error) error {
 	var err error
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	defer signal.Stop(sig)
 
 	errorCount := 0
-	for true {
+	var pause time.Duration
+	for {
 		err = iterationExecutor()
 
 		if errors.Is(err, BreakLoopError) {
 			break
 		}
 
+		pause = config.pauseAfterSuccess
 		if err != nil {
+			pause = config.pauseAfterError
 			fmt.Fprintln(out, getCurrentDatetime()+" "+err.Error())
 
 			errorCount++
@@ -31,12 +40,19 @@ func runMainLoop(config Config, out io.Writer, iterationExecutor func() error) e
 				break
 			}
 
-			time.Sleep(config.pauseAfterError)
 		} else {
 			fmt.Fprintln(out, getCurrentDatetime()+" iteration done success")
 			errorCount = 0
-			time.Sleep(config.pauseAfterSuccess)
 		}
+
+		select {
+		case <-time.After(pause): // nothing
+		case <-sig:
+			fmt.Fprintln(out, "cancelled")
+			return nil
+
+		}
+
 	}
 
 	return err
--- mainloop_test.go
+++ mainloop_test.go
@@ -5,177 +5,220 @@ import (
 	"errors"
 	"github.com/stretchr/testify/assert"
 	"strings"
+	"syscall"
 	"testing"
 	"time"
 )
 
-func TestMainLoopThreeSuccessIterations(t *testing.T) {
-	config := Config{
-		secondaryDekanatDbDSN: "dummy",
-		storageFile:           "dummy",
-		pauseAfterSuccess:     0,
-		pauseAfterError:       0,
-		errorCountToBreak:     3,
-	}
-
-	wantExecutedCount := 3
-	functionExecutedCount := 0
-
-	executeIteration := func() error {
-		functionExecutedCount++
-		if functionExecutedCount >= wantExecutedCount {
-			return BreakLoopError
+func TestMainLoop(t *testing.T) {
+	t.Run("ThreeSuccessIterations", func(t *testing.T) {
+		config := Config{
+			secondaryDekanatDbDSN: "dummy",
+			storageFile:           "dummy",
+			pauseAfterSuccess:     0,
+			pauseAfterError:       0,
+			errorCountToBreak:     3,
 		}
-		return nil
-	}
 
-	var out bytes.Buffer
-	err := runMainLoop(config, &out, executeIteration)
-	output := out.String()
+		wantExecutedCount := 3
+		functionExecutedCount := 0
 
-	assert.Contains(t, output, "iteration done success", "output not contains iteration done success")
-	assert.Equalf(
-		t, wantExecutedCount, functionExecutedCount,
-		"Iteration Function is not executed expected amount times: %q, want %q",
-		functionExecutedCount, wantExecutedCount,
-	)
-	assert.ErrorIsf(
-		t, err, BreakLoopError,
-		"Expected for BreakLoop, got %s.", err,
-	)
-}
-
-func TestMainLoopThreeErrorIterationsAndBreak(t *testing.T) {
-	config := Config{
-		secondaryDekanatDbDSN: "dummy",
-		storageFile:           "dummy",
-		pauseAfterSuccess:     0,
-		pauseAfterError:       0,
-		errorCountToBreak:     3,
-	}
-
-	maxExecutedCount := 5
-	expectedExecutedCount := config.errorCountToBreak
-	functionExecutedCount := 0
-
-	executeIteration := func() error {
-		functionExecutedCount++
-		if functionExecutedCount >= maxExecutedCount {
-			return BreakLoopError
+		executeIteration := func() error {
+			functionExecutedCount++
+			if functionExecutedCount >= wantExecutedCount {
+				return BreakLoopError
+			}
+			return nil
 		}
-		return errors.New("dummy error")
-	}
 
-	var out bytes.Buffer
-	err := runMainLoop(config, &out, executeIteration)
+		var out bytes.Buffer
+		err := runMainLoop(config, &out, executeIteration)
+		output := out.String()
 
-	output := out.String()
+		assert.Contains(t, output, "iteration done success", "output not contains iteration done success")
+		assert.Equalf(
+			t, wantExecutedCount, functionExecutedCount,
+			"Iteration Function is not executed expected amount times: %q, want %q",
+			functionExecutedCount, wantExecutedCount,
+		)
+		assert.ErrorIsf(
+			t, err, BreakLoopError,
+			"Expected for BreakLoop, got %s.", err,
+		)
+	})
 
-	assert.Equalf(
-		t, expectedExecutedCount, functionExecutedCount,
-		"Iteration Function is not executed expected amount times: %q, want %q",
-		functionExecutedCount, expectedExecutedCount,
-	)
-	assert.ErrorIs(
-		t, err, TooManyError,
-		"Expected forTooManyError, got %d.", err,
-	)
-
-	assert.Contains(t, output, "dummy error", "No dummy error in output")
-
-	dummyErrorCount := strings.Count(output, "dummy error")
-	assert.Equalf(
-		t, functionExecutedCount+1, dummyErrorCount,
-		"Not enough amount of dummy error in output. Expected: %d, actual: %d",
-		functionExecutedCount+1, dummyErrorCount,
-	)
-	assert.Contains(t, output, "Too many mistakes", "No Too many mistakes in output")
-}
-
-func TestMainLoopPauseOnSuccess(t *testing.T) {
-	config := Config{
-		secondaryDekanatDbDSN: "dummy",
-		storageFile:           "dummy",
-		pauseAfterSuccess:     100 * time.Microsecond,
-		pauseAfterError:       0,
-		errorCountToBreak:     3,
-	}
-
-	maxExecutedCount := 3
-	expectedExecutedCount := maxExecutedCount
-	functionExecutedCount := 0
-
-	executeIteration := func() error {
-		functionExecutedCount++
-		if functionExecutedCount >= maxExecutedCount {
-			return BreakLoopError
+	t.Run("ThreeErrorIterationsAndBreak", func(t *testing.T) {
+		config := Config{
+			secondaryDekanatDbDSN: "dummy",
+			storageFile:           "dummy",
+			pauseAfterSuccess:     0,
+			pauseAfterError:       0,
+			errorCountToBreak:     3,
 		}
-		return nil
-	}
 
-	var out bytes.Buffer
+		maxExecutedCount := 5
+		expectedExecutedCount := config.errorCountToBreak
+		functionExecutedCount := 0
 
-	start := time.Now()
-	err := runMainLoop(config, &out, executeIteration)
-	executionTime := time.Since(start)
-
-	assert.Equalf(
-		t, expectedExecutedCount, functionExecutedCount,
-		"Iteration Function is not executed expected amount times: %q, want %q",
-		functionExecutedCount, expectedExecutedCount,
-	)
-	assert.ErrorIs(t, err, BreakLoopError, "Expected for BreakLoopError, got %s.", err)
-
-	expectedExecutionTime := config.pauseAfterSuccess * time.Duration(maxExecutedCount-1)
-	assert.Greaterf(
-		t, executionTime, expectedExecutionTime,
-		"Success pause is not applied: Expect for execution greater that %s, actual exectution time: %s",
-		expectedExecutionTime, executionTime,
-	)
-}
-
-func TestMainLoopPauseOnError(t *testing.T) {
-	config := Config{
-		secondaryDekanatDbDSN: "dummy",
-		storageFile:           "dummy",
-		pauseAfterSuccess:     0,
-		pauseAfterError:       100 * time.Microsecond,
-		errorCountToBreak:     3,
-	}
-
-	maxExecutedCount := 3
-	expectedExecutedCount := maxExecutedCount
-	functionExecutedCount := 0
-
-	executeIteration := func() error {
-		functionExecutedCount++
-		if functionExecutedCount >= maxExecutedCount {
-			return BreakLoopError
+		executeIteration := func() error {
+			functionExecutedCount++
+			if functionExecutedCount >= maxExecutedCount {
+				return BreakLoopError
+			}
+			return errors.New("dummy error")
 		}
-		return errors.New("dummy error")
-	}
 
-	var out bytes.Buffer
+		var out bytes.Buffer
+		err := runMainLoop(config, &out, executeIteration)
 
-	start := time.Now()
-	err := runMainLoop(config, &out, executeIteration)
+		output := out.String()
 
-	executionTime := time.Since(start)
+		assert.Equalf(
+			t, expectedExecutedCount, functionExecutedCount,
+			"Iteration Function is not executed expected amount times: %q, want %q",
+			functionExecutedCount, expectedExecutedCount,
+		)
+		assert.ErrorIs(
+			t, err, TooManyError,
+			"Expected forTooManyError, got %d.", err,
+		)
 
-	assert.Equalf(
-		t, expectedExecutedCount, functionExecutedCount,
-		"Iteration Function is not executed expected amount times: %q, want %q",
-		functionExecutedCount, expectedExecutedCount,
-	)
-	assert.ErrorIs(t, err, BreakLoopError, "Expected for ExitCodeLoopIsBroken, got %d.", err)
+		assert.Contains(t, output, "dummy error", "No dummy error in output")
 
-	expectedExecutionTime := config.pauseAfterError * time.Duration(maxExecutedCount-1)
+		dummyErrorCount := strings.Count(output, "dummy error")
+		assert.Equalf(
+			t, functionExecutedCount+1, dummyErrorCount,
+			"Not enough amount of dummy error in output. Expected: %d, actual: %d",
+			functionExecutedCount+1, dummyErrorCount,
+		)
+		assert.Contains(t, output, "Too many mistakes", "No Too many mistakes in output")
+	})
 
-	assert.Greaterf(
-		t, executionTime, expectedExecutionTime,
-		"Success pause is not applied: Expect for execution greater that %s, actual exectution time: %s",
-		expectedExecutionTime, executionTime,
-	)
+	t.Run("PauseOnSuccess", func(t *testing.T) {
+		config := Config{
+			secondaryDekanatDbDSN: "dummy",
+			storageFile:           "dummy",
+			pauseAfterSuccess:     100 * time.Microsecond,
+			pauseAfterError:       0,
+			errorCountToBreak:     3,
+		}
+
+		maxExecutedCount := 3
+		expectedExecutedCount := maxExecutedCount
+		functionExecutedCount := 0
+
+		executeIteration := func() error {
+			functionExecutedCount++
+			if functionExecutedCount >= maxExecutedCount {
+				return BreakLoopError
+			}
+			return nil
+		}
+
+		var out bytes.Buffer
+
+		start := time.Now()
+		err := runMainLoop(config, &out, executeIteration)
+		executionTime := time.Since(start)
+
+		assert.Equalf(
+			t, expectedExecutedCount, functionExecutedCount,
+			"Iteration Function is not executed expected amount times: %q, want %q",
+			functionExecutedCount, expectedExecutedCount,
+		)
+		assert.ErrorIs(t, err, BreakLoopError, "Expected for BreakLoopError, got %s.", err)
+
+		expectedExecutionTime := config.pauseAfterSuccess * time.Duration(maxExecutedCount-1)
+		assert.Greaterf(
+			t, executionTime, expectedExecutionTime,
+			"Success pause is not applied: Expect for execution greater that %s, actual exectution time: %s",
+			expectedExecutionTime, executionTime,
+		)
+	})
+
+	t.Run("PauseOnError", func(t *testing.T) {
+		config := Config{
+			secondaryDekanatDbDSN: "dummy",
+			storageFile:           "dummy",
+			pauseAfterSuccess:     0,
+			pauseAfterError:       100 * time.Microsecond,
+			errorCountToBreak:     3,
+		}
+
+		maxExecutedCount := 3
+		expectedExecutedCount := maxExecutedCount
+		functionExecutedCount := 0
+
+		executeIteration := func() error {
+			functionExecutedCount++
+			if functionExecutedCount >= maxExecutedCount {
+				return BreakLoopError
+			}
+			return errors.New("dummy error")
+		}
+
+		var out bytes.Buffer
+
+		start := time.Now()
+		err := runMainLoop(config, &out, executeIteration)
+
+		executionTime := time.Since(start)
+
+		assert.Equalf(
+			t, expectedExecutedCount, functionExecutedCount,
+			"Iteration Function is not executed expected amount times: %q, want %q",
+			functionExecutedCount, expectedExecutedCount,
+		)
+		assert.ErrorIs(t, err, BreakLoopError, "Expected for ExitCodeLoopIsBroken, got %d.", err)
+
+		expectedExecutionTime := config.pauseAfterError * time.Duration(maxExecutedCount-1)
+
+		assert.Greaterf(
+			t, executionTime, expectedExecutionTime,
+			"Success pause is not applied: Expect for execution greater that %s, actual exectution time: %s",
+			expectedExecutionTime, executionTime,
+		)
+	})
+
+	t.Run("Sigterm", func(t *testing.T) {
+		config := Config{
+			secondaryDekanatDbDSN: "dummy",
+			storageFile:           "dummy",
+			pauseAfterSuccess:     3 * time.Second,
+			pauseAfterError:       0,
+			errorCountToBreak:     3,
+		}
+
+		maxExecutedCount := 2
+		expectedExecutedCount := 1
+		functionExecutedCount := 0
+
+		executeIteration := func() error {
+			functionExecutedCount++
+			if functionExecutedCount >= maxExecutedCount {
+				return BreakLoopError
+			}
+			return nil
+		}
+
+		var out bytes.Buffer
+
+		go func() {
+			time.Sleep(time.Microsecond * 50)
+			syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+		}()
+
+		err := runMainLoop(config, &out, executeIteration)
+
+		assert.Equalf(
+			t, expectedExecutedCount, functionExecutedCount,
+			"Iteration Function is not executed expected amount times: %q, want %q",
+			functionExecutedCount, expectedExecutedCount,
+		)
+		assert.NoError(t, err, "Not expected error %s.", err)
+
+		assert.Contains(t, out.String(), "cancelled", "No `canceled` string in output")
+	})
 }
 
 func TestGetCurrentDatetime(t *testing.T) {
